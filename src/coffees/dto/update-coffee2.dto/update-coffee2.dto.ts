23e343d7f7d5c47216b544aabe2e9f0e9f0d370e
Martin Couso <martin.couso@equinox.com>
2023-01-06 16:12:30 -0300

    fix: remove test dtos

--- src/coffees/dto/update-coffee2.dto/update-coffee2.dto.ts
+++
@@ -1,1 +0,0 @@
-export class UpdateCoffee2Dto {}
--- src/coffees/dto/update-coffee3.dto.ts/update-coffee3.dto.ts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UpdateCoffee3DtoTs } from './update-coffee3.dto.ts';
-
-describe('UpdateCoffee3DtoTs', () => {
-  it('should be defined', () => {
-    expect(new UpdateCoffee3DtoTs()).toBeDefined();
-  });
-});
